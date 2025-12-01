b316c2be2e6b01b6543253e87a330404e6de66f2
kiomnd2 <kiomnd2@gmail.com>
2025-12-01 20:16:34 +0900

    first commit

---
+++ src/main/java/kr/kiomn2/newsletter/bigtraffic/BigTrafficApplication.java
@@ -0,0 +1,13 @@
+package kr.kiomn2.newsletter.bigtraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigTrafficApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigTrafficApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kiomn2/newsletter/bigtraffic/BigTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kiomn2.newsletter.bigtraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigTrafficApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
